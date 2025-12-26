399aea019a33f2cfc23fd8dbd73ece56598ccf62
code-with-hammad <hammad06999@gmail.com>
2025-12-27 04:53:15 +0500

    Update production URL in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hackathon-physical-ai-humanoid-textbook-kwccwwddx.vercel.app/',
+  url: 'https://hackathon-physical-ai-humanoid-textbook-mdwnkqts7.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
